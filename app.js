29e23e66bd79e8d0e5964c9fae41a95bc23b1fb6
mrdanishalam <mddanish960855@gmail.com>
2025-11-28 19:47:40 +0530

    exploring GET , POST , DELETE and also gitignore

--- app.js
+++ app.js
@@ -2,24 +2,43 @@ const express = require('express');
 
 const app = express();
 
+//This will only handle get call to /test
+app.get("/user",(req,res)=>{
+res.send({firstname:"Danish",lastname:"hassan"})
+});
 
+app.post("/user",(req,res)=>{
+res.send("Data SAved successfullt in DB")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Data Deleted Successfully")
+});
+
+//This will match all HTTP modules method /test
 app.use("/test",(req,res)=>{
 res.send(" bhai logic seekh rha hun");
 
 });
 
-app.use("/sutta",(req,res)=>{
-res.send(" bhai time kahatam sutta ab nhi ");
+// app.use("/sutta",(req,res)=>{
+// res.send(" bhai time kahatam sutta ab nhi ");
 
-});
+// });
 
-app.use("/ganja",(req,res)=>{
-res.send(" bhai pay kar pahle");
+// app.use("/ganja",(req,res)=>{
+// res.send(" bhai pay kar pahle");
 
-});
-app.use( (req, res) => {
-    res.send("Hello from Dashboard se");
-});
+// });
+
+// app.use("/",(req,res)=>{
+// res.send(" bhai dekh koi bhadiyan sa daal le");
+
+// });
+
+// app.use( (req, res) => {
+//     res.send("Hello from Dashboard se");
+// });
 
 
 
